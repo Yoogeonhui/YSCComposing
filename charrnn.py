86c1d7c77241ba47f6942855a534f57cfb596d01
pc <jrguyyoo@gmail.com>
2017-09-21 22:45:55 +0900

    TensorBoard가 남음

--- charrnn.py
+++ charrnn.py
@@ -49,11 +49,11 @@ def get_batch(what='train', batchsize = 50):
     return out_batch
 
 
-def linear(inp, out_dim):
-    from_dim = int(inp.shape[1])
-    print(from_dim)
-    lin_w = tf.get_variable('lin_w', [from_dim,out_dim], dtype = tf.float32, initializer = tf.truncated_normal_initializer)
-    lin_b = tf.get_variable('lin_b', [out_dim], dtype = tf.float32, initializer = tf.truncated_normal_initializer)
+def linear(inp, out_dim, scope_name, reuse=False):
+    with tf.variable_scope(scope_name, reuse=reuse):
+        from_dim = int(inp.shape[1])
+        lin_w = tf.get_variable('lin_w', [from_dim,out_dim], dtype = tf.float32, initializer = tf.truncated_normal_initializer)
+        lin_b = tf.get_variable('lin_b', [out_dim], dtype = tf.float32, initializer = tf.truncated_normal_initializer)
     return tf.matmul(inp, lin_w)+lin_b
 
 
@@ -79,11 +79,21 @@ def generator(z_prior, z_size):
     return output
 
 
+def get_dis_res(two_added_mean, scope_name, reuse=False):
+    with tf.variable_scope(scope_name, reuse=reuse):
+        dis_res = tf.reshape(two_added_mean, [-1, lstm_dim])
+        dis_res = linear(dis_res, 1, scope_name+'_lin', reuse=reuse)
+        dis_res = tf.sigmoid(dis_res)
+        dis_res = tf.reshape(dis_res, [batch_size, max_song_size])
+        dis_res = tf.reduce_mean(dis_res, axis=1)
+    return dis_res
+
+
 def main():
     global EPOCH
     init_batch()
-    #z_in = tf.placeholder(tf.float32, shape = [batch_size,max_song_size,lstm_dim])
-    #z_size = tf.placeholder(tf.int32, shape= [None])
+    # z_in = tf.placeholder(tf.float32, shape = [batch_size,max_song_size,lstm_dim])
+    # z_size = tf.placeholder(tf.int32, shape= [None])
     save_cnt = tf.Variable(0, dtype=tf.int32, name='global_step')
     z_in = tf.random_normal([batch_size, max_song_size, lstm_dim])
     z_size = tf.constant(max_song_size, dtype= tf.int32, shape=[batch_size])
@@ -92,33 +102,37 @@ def main():
     with tf.variable_scope("Generator") as scope:
         gen = generator(z_in, z_size)
         gen = tf.reshape(gen,[-1, lstm_dim])
-        gen = linear(gen, song_feature)
+        gen = linear(gen, song_feature, 'gen_linear')
         gen_res = tf.reshape(gen, [-1, max_song_size, song_feature])
         generator_variables = [v for v in tf.global_variables()
-                          if v.name.startswith(scope.name)]
+                               if v.name.startswith(scope.name)]
 
     with tf.variable_scope("discriminator") as scope2:
-        gen_converted = linear(gen, lstm_dim)
+        gen_converted = linear(gen, lstm_dim, 'dis_lin')
         print(gen_converted.shape)
         gen_converted = tf.reshape(gen_converted,[-1, max_song_size, lstm_dim])
         dis_gen_fw, dis_gen_bw = discriminator(gen_converted, z_size)
-        x_in_cv = tf.reshape(x_in, [-1, song_feature])
+        dis_calc_in_loss_gen = (dis_gen_fw+dis_gen_bw)/2
+        dis_res_gen = get_dis_res(dis_calc_in_loss_gen, 'dis_res')
         scope2.reuse_variables()
-        x_in_cv = linear(x_in_cv, lstm_dim)
+        x_in_cv = tf.reshape(x_in, [-1, song_feature])
+        x_in_cv = linear(x_in_cv, lstm_dim, 'dis_lin', reuse=True)
         print(x_in_cv)
         x_in_cv = tf.reshape(x_in_cv, [-1,max_song_size, lstm_dim])
         dis_real_fw, dis_real_bw = discriminator(x_in_cv, x_size)
+        dis_calc_in_loss_real = (dis_real_fw+dis_real_bw)/2
+        dis_res_real = get_dis_res(dis_calc_in_loss_real, 'dis_res', reuse=True)
+        #D Loss를 위한 식
+
         dis_variables = [v for v in tf.global_variables()
                                if v.name.startswith(scope2.name)]
-
+    print(dis_real_fw.shape)
     print('genvars: ', generator_variables)
     print('disvars: ', dis_variables)
     # Generator Feature Matching Loss
-    g_fm_loss = tf.reduce_sum(tf.squared_difference(dis_real_fw, dis_gen_fw)) + \
-                tf.reduce_sum(tf.squared_difference(dis_real_bw, dis_gen_bw))
-    dis_calc_in_loss_real = (dis_real_fw+dis_real_bw)/2
-    dis_calc_in_loss_gen = (dis_gen_fw+dis_gen_bw)/2
-    d_loss = tf.reduce_mean(-tf.log(tf.clip_by_value(dis_calc_in_loss_real,1e-4,1))-tf.log(tf.clip_by_value(1.0 - dis_calc_in_loss_gen,1e-4,1)))
+
+    g_fm_loss = tf.reduce_sum(tf.squared_difference(dis_calc_in_loss_real, dis_calc_in_loss_gen))
+    d_loss = tf.reduce_mean(-tf.log(tf.clip_by_value(dis_res_real,1e-4,1))-tf.log(tf.clip_by_value(1.0 - dis_res_gen,1e-4,1)))
     if isAdam:
         trainG = tf.train.AdamOptimizer(AdamLearningrate).minimize(g_fm_loss, var_list = generator_variables)
         trainD = tf.train.AdamOptimizer(AdamLearningrate).minimize(d_loss, var_list = dis_variables)
@@ -156,8 +170,10 @@ def main():
                 '''z_input = tf.random_uniform(shape = [batch_size, max_song_size, lstm_dim], minval = 0.0, maxval = 1.0,
                                             dtype=tf.float32)'''
                 train_dict = {x_in: song_data, x_size: song_getsoo}
-                train_g_loss, train_d_loss, _, _, generated = sess.run([g_fm_loss, d_loss, trainG, trainD, gen], feed_dict=train_dict)
-
+                train_g_loss, train_d_loss, _, _, generated, dis_res_see_gen, dis_res_see_real = sess.run([g_fm_loss, d_loss, trainD, trainG, gen_res, dis_res_gen, dis_res_real],
+                                                                       feed_dict=train_dict)
+                print('dis res real', dis_res_real)
+                print('dis res gen', dis_res_gen)
                 print('Generated', generated[0:10])
                 if cnt_step % print_batch == 0:
                     print('epoch: ', epoch, ' cnt_step_num: ', cnt_step, ' G loss: ', train_g_loss,' D loss: ', train_d_loss)
